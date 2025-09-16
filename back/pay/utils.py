0eeebb4afa15fe7ecbdc18da2d87e6b37f66be57
8lue <hkahk@naver.com>
2025-09-16 16:03:29 +0900

    250916_하껑_token 발급 추가

---
+++ back/pay/utils.py
@@ -0,0 +1,11 @@
+import jwt
+from django.conf import settings
+
+def verify_token(token):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        return payload
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
--- back/pay/views.py
+++ back/pay/views.py
@@ -5,9 +5,15 @@ from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 from .models import Order, Menu
 from rest_framework import status
+import datetime
+from django.conf import settings
+import jwt
 
 from .serializers import OrderSerializer, MenuSerializer
 
+from .utils import verify_token
+from django.http import JsonResponse, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
 
 class OrderViewSet(generics.ListCreateAPIView): 
     queryset = Order.objects.all()
@@ -18,6 +24,10 @@ class OrderViewSet(generics.ListCreateAPIView):
     # 귯결~^^ 
     def get_queryset(self):
         queryset = super().get_queryset()
+
+        # 추가: 결제 완료 됐고 아직 처리 안 된 주문 
+        queryset = queryset.filter(is_paid=True, is_done=False)
+
         # 쿼리스트링으로 받은 값 가져오기
         is_paid = self.request.query_params.get('is_paid')
         is_done = self.request.query_params.get('is_done')
@@ -46,3 +56,27 @@ class OrderReadyView(APIView):
         serializer = OrderSerializer(order)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
+# 보안 
+SECRET_KEY = settings.SECRET_KEY
+
+
+@csrf_exempt
+def generate_token(request, table_id):
+    exp_time = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=30)
+    payload = {
+        "table_id": table_id,
+        "exp": exp_time
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    return JsonResponse({"token": token})
+
+
+def order_page(request):
+    token = request.GET.get("token")
+    payload = verify_token(token)
+    if not payload:
+        return HttpResponseForbidden("토큰이 유효하지 않습니다.")
+    
+    table_id = payload["table_id"]
+    return JsonResponse({"table_id": payload["table_id"], "order":"sample"})
